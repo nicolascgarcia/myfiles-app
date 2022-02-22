99f55722be8b9891ec6f4fa69f3ea3c6a5dc2042
Nicolas Garcia <ni.car.garcia@gmail.com>
2022-02-22 20:54:31 -0300

    feat(app): added api context

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'react-native';
+import FlashMessage from 'react-native-flash-message';
 import MainNavigator from '@/routes';
+import APIManager from '@/context/APIContext';
 
 export default function App() {
 	return (
-		<NavigationContainer>
-			<StatusBar barStyle='dark-content' backgroundColor='white' />
-			<MainNavigator />
-		</NavigationContainer>
+		<APIManager>
+			<NavigationContainer>
+				<StatusBar barStyle='dark-content' backgroundColor='white' />
+				<MainNavigator />
+				<FlashMessage position='top' />
+			</NavigationContainer>
+		</APIManager>
 	);
 }
