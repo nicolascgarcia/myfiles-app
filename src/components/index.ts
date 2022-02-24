7b115e708b3001a8641280cc5109a52c81d92c41
Nicolas Garcia <ni.car.garcia@gmail.com>
2022-02-23 21:50:00 -0300

    feat(components): added new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,17 @@ import Text from './Text';
 import ListSwitch from './ListSwitch';
 import SortButton from './SortButton';
 import CellCard from './CellCard';
+import BottomSheet from './BottomSheet';
+import CustomTextInput from './CustomTextInput';
+import MainButton from './MainButton';
 
-export { Container, Text, ListSwitch, SortButton, CellCard };
+export {
+	Container,
+	Text,
+	ListSwitch,
+	SortButton,
+	CellCard,
+	BottomSheet,
+	CustomTextInput,
+	MainButton,
+};
